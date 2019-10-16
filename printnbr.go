822883831fdb619e0c6f4e2cb9f413e959b659f6
medetas <masylzhan07@gmail.com>
2019-10-17 00:17:35 +0600

    q027

--- printnbr.go
+++ printnbr.go
@@ -2,29 +2,41 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func Recur(n int) {
+func Recurneg(n int) {
 	if n == 0 {
 		return
+	} else {
+		c := '0'
+		for i := 0; i > n%10; i-- {
+			c++
+		}
+		Recurneg(n / 10)
+		z01.PrintRune(c)
 	}
-	c := '0'
-	for i := 0; i < n%10; i++ {
-		c++
-	}
-	n = n / 10
-	Recur(n)
-	z01.PrintRune(c)
+}
 
+func Recurpos(n int) {
+	if n == 0 {
+		return
+	} else {
+		c := '0'
+		for i := 0; i < n%10; i++ {
+			c++
+		}
+		Recurpos(n / 10)
+		z01.PrintRune(c)
+	}
 }
 
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune(45)
-		x := -1 * n
-		Recur(x)
+		Recurneg(n * -1)
+
 	} else if n == 0 {
 		z01.PrintRune(48)
-		Recur(n)
+
 	} else {
-		Recur(n)
+		Recurpos(n)
 	}
 }
