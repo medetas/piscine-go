e01962dc40a08bbbe06a88fbaa70daf451f58219
medetas <masylzhan07@gmail.com>
2019-10-17 00:00:48 +0600

    asdfs

--- printnbr.go
+++ printnbr.go
@@ -9,25 +9,22 @@ func Recur(n int) {
 	c := '0'
 	for i := 0; i < n%10; i++ {
 		c++
-	} 
+	}
 	n = n / 10
 	Recur(n)
 	z01.PrintRune(c)
-	
+
 }
 
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune(45)
-	x := -1 * n
+		x := -1 * n
 		Recur(x)
-	}else if n == 0 {
+	} else if n == 0 {
 		z01.PrintRune(48)
 		Recur(n)
-	}else {
-		Recur(n) 			
+	} else {
+		Recur(n)
 	}
 }
-	
-
-
