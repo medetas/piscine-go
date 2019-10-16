def9ce442ca81992e85f4911a4089b253891cb97
medetas <masylzhan07@gmail.com>
2019-10-16 23:11:15 +0600

    q027

---
+++ printnbr.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune(45)
+		n = n * -1
+	}
+	a := n % 10
+	n = n / 10
+	var c rune = '0'
+	if a > 0 {
+		for i := 0; i < a; i++ {
+			c++
+		}
+		PrintNbr(n)
+		z01.PrintRune(c)
+
+	}
+
+}
+
+
