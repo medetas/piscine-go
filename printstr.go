de45ad1f39ffa40fc5c030a6b1c3e3bd360c9c18
medetas <masylzhan07@gmail.com>
2019-10-17 20:09:43 +0600

    q0305

--- printstr.go
+++ printstr.go
@@ -1,10 +1,15 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func PrintStr(str string) {
 	slice := []string{
 		str,
 	}
 	for _, word := range slice {
-		print(word)
+		for _, char := range word {
+			z01.PrintRune(char)
+		}
 	}
+	z01.PrintRune(10)
 }
