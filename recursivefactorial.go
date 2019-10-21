3793000104de2c6cc57b3cbe61e84fdd7ac37ea0
medetas <masylzhan07@gmail.com>
2019-10-21 16:06:03 +0600

    q0402

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,9 +1,9 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 {
+	if nb == 0 {
 		return 1
-	} else if nb > 1 && nb < 18 {
+	} else if nb > 0 && nb < 18 {
 		return nb * RecursiveFactorial(nb-1)
 	} else {
 		return 0
