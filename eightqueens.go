f480c4737ddd1b2253a55ae2a433aa2af819e51f
medetas <masylzhan07@gmail.com>
2019-10-21 22:50:47 +0600

    q0409

---
+++ eightqueens.go
@@ -0,0 +1,47 @@
+package main
+
+import "github.com/01-edu/z01"
+
+const N int = 9
+
+var position [N]int
+
+func Convert(n int) rune {
+	charX := '0'
+	for i := 0; i < n; i++ {
+		charX++
+	}
+	return charX
+}
+
+func isSafe(qn int, rp int) bool {
+	for i := 1; i < qn; i++ {
+		orp := position[i]
+		if orp == rp || orp == rp-(qn-i) || orp == rp+(qn-i) {
+			return false
+		}
+	}
+	return true
+}
+
+func Solve() {
+	k := 1
+	if k == N {
+		for i := 1; i < N; i++ {
+			r := position[i]
+			z01.PrintRune(Convert(r))
+		}
+		z01.PrintRune(10)
+	} else {
+		for i := 1; i < N; i++ {
+			if isSafe(k, i) {
+				position[k] = i
+				Solve(k + 1)
+			}
+		}
+	}
+}
+
+func EightQueens() {
+	Solve(1)
+}
