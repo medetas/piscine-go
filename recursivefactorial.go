46a6f3fe5298b90cec3f0237be63f0504629921d
medetas <masylzhan07@gmail.com>
2019-10-21 16:04:02 +0600

    q0402

---
+++ recursivefactorial.go
@@ -0,0 +1,11 @@
+package main
+
+func RecursiveFactorial(nb int) int {
+	if nb == 1 {
+		return 1
+	} else if nb > 1 && nb < 18 {
+		return nb * RecursiveFactorial(nb-1)
+	} else {
+		return 0
+	}
+}
