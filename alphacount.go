57501f55bedc6a88dc72174ef3c96b9b1c8b6842
medetas <masylzhan07@gmail.com>
2019-10-22 17:22:14 +0600

    q0501

---
+++ alphacount.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AlphaCount(str string) int {
+	count := 0
+	for _, letter := range str {
+		if (letter < 'Z' && letter > 'A') || (letter < 'z' && letter > 'a') {
+			count++
+		}
+	}
+	return count
+}
