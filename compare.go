0c39146ac2892bef73b398780a08319b2d8967b5
medetas <masylzhan07@gmail.com>
2019-10-23 00:24:09 +0600

    q0508

---
+++ compare.go
@@ -0,0 +1,31 @@
+package piscine
+
+func StrLen(str string) int {
+	a := 0
+	slice := []rune(str)
+	for index := range slice {
+		a = index
+	}
+	return a + 1
+
+}
+
+func Compare(a, b string) int {
+	str1 := ""
+	index := -1
+	if a == b {
+		return 0
+	} else {
+		for i := 0; i < StrLen(a)-StrLen(b); i++ {
+			for j := 0; j < StrLen(b); j++ {
+				str1 = str1 + string(a[j+i])
+			}
+			if str1 == b {
+				index = 1
+				break
+			}
+			str1 = ""
+		}
+		return index
+	}
+}
