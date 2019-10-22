763d8bd4d61b28bcd7924bb157d5237e2654b1e0
medetas <masylzhan07@gmail.com>
2019-10-22 22:54:37 +0600

    q0505

--- nrune.go
+++ nrune.go
@@ -1,6 +1,20 @@
 package piscine
 
+func StrLen(str string) int {
+	a := 0
+	slice := []rune(str)
+	for index := range slice {
+		a = index
+	}
+	return a + 1
+
+}
+
 func NRune(s string, n int) rune {
 	str := []rune(s)
-	return rune(str[n-1])
+	if s != "" && n > 0 {
+		return rune(str[n-1])
+	} else {
+		return ' '
+	}
 }
