a2cbc12b3305c685af76857b1a40a8ca317ade64
medetas <masylzhan07@gmail.com>
2019-10-22 23:14:50 +0600

    q0506

---
+++ lastrune.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/01-edu/z01"
+
+/*func StrLen(str string) int {
+	a := 0
+	slice := []rune(str)
+	for index := range slice {
+		a = index
+	}
+	return a + 1
+
+}*/
+
+func LastRune(s string) rune {
+	str := []rune(s)
+	if s != "" {
+		return str[StrLen(s)-1]
+	}
+	return 0
+}
