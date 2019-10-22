4f2751d768f83f0c04b4aa527e33f90eb3a17cb6
medetas <masylzhan07@gmail.com>
2019-10-22 21:52:24 +0600

    q0503

---
+++ trimatoi.go
@@ -0,0 +1,64 @@
+package piscine
+
+func StrLenn(str string) int {
+	a := 0
+	slice := []rune(str)
+	for index := range slice {
+		a = index
+	}
+	return a + 1
+
+}
+
+func Convertt(s rune) int {
+	a := 0
+	for true {
+		if s == '0' {
+			break
+		}
+		a++
+		s--
+	}
+	return a
+}
+
+func Countt(s []rune) int {
+	sum := 0
+	m := 1
+	for _, letter := range s {
+		if letter > '0' && letter < '9' {
+			sum = sum + Convert(letter)*m
+			m = m * 10
+		} else {
+			continue
+		}
+	}
+	return sum
+}
+
+func TrimAtoi(s string) int {
+	check := 0
+	sign := 1
+	var slice []rune
+	if s == "" {
+		return 0
+	}
+	for ind := range s {
+		if s[ind] >= '0' && s[ind] <= '9' {
+			check++
+		} else if s[ind] == '-' && check == 0 {
+			sign = -1
+		}
+	}
+
+	slice = []rune(s)
+
+	a := StrLen(string(slice))
+
+	slice2 := []rune(s)
+	for index, letter := range slice {
+		slice2[a-index-1] = letter
+	}
+	result := Count(slice2)
+	return result * sign
+}
