36c8eb47c200dfaa45ee32d37bab4fc61f84970e
medetas <masylzhan07@gmail.com>
2019-10-23 00:11:40 +0600

    q0507

---
+++ index.go
@@ -0,0 +1,28 @@
+package main
+
+/*func StrLen(str string) int {
+	a := 0
+	slice := []rune(str)
+	for index := range slice {
+		a = index
+	}
+	return a+1
+
+}*/
+
+func Index(s string, toFind string) int {
+	str1 := ""
+	index := -1
+	for i := 0; i < StrLen(s)-StrLen(toFind); i++ {
+		for j := 0; j < StrLen(toFind); j++ {
+			str1 = str1 + string(s[j+i])
+		}
+		if str1 == toFind {
+			index = i
+			break
+		}
+		str1 = ""
+	}
+	return index
+
+}
