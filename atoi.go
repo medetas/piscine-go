d9f55d08c14c38c4c9c004111d28c9541bbe17c5
medetas <masylzhan07@gmail.com>
2019-10-23 16:14:38 +0600

    q03atoi

--- atoi.go
+++ atoi.go
@@ -1,47 +1,29 @@
 package piscine
 
-func Convert3(s rune) int {
-	a := 0
-	for true {
-		if s == '0' {
-			break
-		}
-		a++
-		s--
+func Atoi(s string) int {
+	//a := 0
+	sign := 1
+	var slice []rune
+	if s == "" {
+		return 0
+	} else if s[0] == '-' {
+		sign = -1
+		s = s[1:]
+	} else if s[0] == '+' {
+		s = s[1:]
 	}
-	return a
-}
+	slice = []rune(s)
+	//fmt.Println(s)
+	/*for _, letter := range slice {
+		fmt.Println(string(letter))
+	}*/
 
-func Count3(s []rune) int {
-	sum := 0
-	m := 1
-	slice := []rune(s)
-	for _, letter := range slice {
-		if letter < '0' || letter > '9' {
-			sum = 0
-			break
-		}
-		sum = sum + Convert(letter)*m
-		m = m * 10
-	}
-	return sum
-}
-
-func Atoi3(s string) int {
-	a := 0
-	slice := []rune(s)
-	for word := range slice {
-		a = word
-	}
+	a := StrLen(string(slice))
 
 	slice2 := []rune(s)
 	for index, letter := range slice {
-		if letter == '+' || letter == '-' {
-			continue
-		} else {
-			slice2[a-index] = letter
-		}
+		slice2[a-index-1] = letter
 	}
 	result := Count(slice2)
-	return result
+	return result * sign
 }
