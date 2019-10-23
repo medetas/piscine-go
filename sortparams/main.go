c3f86682fb7440c76753b6616d6672fdcf3b560f
medetas <masylzhan07@gmail.com>
2019-10-23 21:53:07 +0600

    q0604

---
+++ sortparams/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "os"
+import "github.com/01-edu/z01"
+
+func ArgLen(str []string) int {
+	a := 0
+	slice := str
+	for index := range slice {
+		a = index
+	}
+	return a + 1
+}
+
+
+
+func main() {
+	arr := os.Args[1:]
+	s := ArgLen(arr)
+	
+	for i := s; i > 0; i-- {
+		for j := 1; j < i; j++ {
+			if arr[j-1] > arr[j] {
+				min := arr[j]
+				arr[j] = arr[j-1]
+				arr[j-1] = min
+			}
+		}
+	}
+	for index := range arr {
+		for _, index2 := range arr[index] {
+			z01.PrintRune(index2)
+		}
+		z01.PrintRune(10)
+	}
+}
