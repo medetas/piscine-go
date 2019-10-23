a33f0c4406974add05dd239de0563137ce7fa6d7
medetas <masylzhan07@gmail.com>
2019-10-23 18:48:07 +0600

    q05018

--- basicjoin.go
+++ basicjoin.go
@@ -1,8 +1,17 @@
 package piscine
 
+func StrLennn(str []string) int {
+	a := 0
+	//slice := []rune(str)
+	for index := range str {
+		a = index
+	}
+	return a + 1
+}
+
 func BasicJoin(strs []string) string {
 	s := ""
-	for i := 0; i < len(strs); i++ {
+	for i := 0; i < StrLennn(strs); i++ {
 		s = s + strs[i]
 	}
 	return s
