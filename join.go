cee9847ee8d55e40824a22b522be61cc8ecf62cf
medetas <masylzhan07@gmail.com>
2019-10-23 18:57:07 +0600

    q05019

--- join.go
+++ join.go
@@ -3,7 +3,11 @@ package piscine
 func Join(strs []string, sep string) string {
 	s := ""
 	for i := 0; i < StrLennn(strs); i++ {
-		s = s + strs[i] + sep
+		if i != StrLennn(strs)-1 {
+			s = s + strs[i] + sep
+		} else {
+			s = s + strs[i]
+		}
 	}
 	return s
 }
