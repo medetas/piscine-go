8c10bd0d376b7faedaf62b0f950b1bd7816ccbda
medetas <masylzhan07@gmail.com>
2019-10-23 17:53:54 +0600

    q05011

--- capitalize.go
+++ capitalize.go
@@ -9,7 +9,7 @@ func Capitalize(s string) string {
 			if letter >= 97 && letter <= 122 && (slice[index-1] < 65 || slice[index-1] > 90) && (slice[index-1] < 97 || slice[index-1] > 122) && (slice[index-1] < 48 || slice[index-1] > 57) {
 				letter = letter - 32
 				//fmt.Println(slice[index-1])
-			} else if letter >= 65 && letter <= 90 && ((slice[index-1] >= 65 && slice[index-1] <= 90) || (slice[index-1] >= 48 && slice[index-1] <= 57) || (slice[index-1] >= 97 || slice[index-1] <= 122)) {
+			} else if letter >= 65 && letter <= 90 && ((slice[index-1] >= 65 && slice[index-1] <= 90) || (slice[index-1] >= 48 && slice[index-1] <= 57) || (slice[index-1] >= 97 && slice[index-1] <= 122)) {
 				letter = letter + 32
 			}
 
