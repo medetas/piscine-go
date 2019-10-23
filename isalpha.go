f169d73d32298e2367125da6af38afc3c0fa1951
medetas <masylzhan07@gmail.com>
2019-10-23 18:15:58 +0600

    q05012

--- isalpha.go
+++ isalpha.go
@@ -1,24 +1,14 @@
 package piscine
 
-func Capitalize(s string) string {
-	slice := []rune(s)
-	str := ""
+func IsAlpha(str string) bool {
+	slice := []rune(str)
+	b := true
 
-	for index, letter := range slice {
-		if index > 0 {
-			if letter >= 97 && letter <= 122 && (slice[index-1] < 65 || slice[index-1] > 90) && (slice[index-1] < 97 || slice[index-1] > 122) && (slice[index-1] < 48 || slice[index-1] > 57) {
-				letter = letter - 32
-				//fmt.Println(slice[index-1])
-			} else if letter >= 65 && letter <= 90 && ((slice[index-1] >= 65 && slice[index-1] <= 90) || (slice[index-1] >= 48 && slice[index-1] <= 57) || (slice[index-1] >= 97 && slice[index-1] <= 122)) {
-				letter = letter + 32
-			}
-
-		} else if letter >= 97 && letter <= 122 {
-			letter = letter - 32
-			//fmt.Println(slice[index])
+	for _, letter := range slice {
+		if (letter < 48 || letter > 57) && (letter < 65 || letter > 90) && (letter < 97 || letter > 122) {
+			b = false
+			break
 		}
-		str = str + string(letter)
-
 	}
-	return str
+	return b
 }
