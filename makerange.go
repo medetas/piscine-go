c7917ef4e58377918c9c5a645c21f8e7ca3e98a1
medetas <masylzhan07@gmail.com>
2019-10-24 19:04:41 +0600

    q0702

--- makerange.go
+++ makerange.go
@@ -2,14 +2,14 @@ package piscine
 
 func MakeRange(min, max int) []int {
 	var retarr []int
-	if min < max {
+	if min >= max {
+		return retarr
+	} else {
 		array := make([]int, max)
 		for i := min; i < max; i++ {
 			array[i] = i
 		}
 		retarr = array[min:]
-	} else {
-		return retarr
 	}
 	return retarr
 }
